7f686d7547910d3814532f2e5389addfefd7bb72
Anastasiya <tiger.ks@mail.ru>
2024-07-20 16:14:14 +0300

    Add files via upload

---
+++ homework2.3.py
@@ -0,0 +1,10 @@
+my_list = [42, 69, 0, 322, 13, 99, -5, 9, 8, 7, -6, 5]
+a = [-0]
+while my_list > a :
+     print(my_list[0])
+     print(my_list[1])
+     print(my_list[3])
+     print(my_list[4])
+     print(my_list[5])
+     break
+     continue
